27a17a053b9e8e9f2e77b8f6742b55739102c953
Daniel Terra <danielterra24@hotmail.com>
2025-09-04 21:10:39 -0300

    Add README and script to launch draw.io
    - Created a README file with instructions to launch CoppeliaSim.
    - Added a script (run-drawio.sh) to easily launch draw.io from the robotica movel folder.

---
+++ readme.md
@@ -0,0 +1,2 @@
+cd /home/danielterra/Documents/programs/CoppeliaSim_Edu_V4_9_0_rev6_Ubuntu20_04
+./coppeliaSim.sh
---
+++ run-drawio.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Easy launcher for draw.io from the robotica movel folder
+echo "Launching draw.io..."
+~/Applications/draw.io.AppImage &
+echo "draw.io started in background"
